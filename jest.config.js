eaedfbaeed59340a67b9ddd5ebda335c009ce383
Marcelo Fabiano <marcelofabianov@gmail.com>
2022-06-17 00:43:49 -0300

    chore: update config jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 const { resolve } = require('path');
 const root = resolve(__dirname);
+
 module.exports = {
     rootDir: root,
     displayName: 'root-tests',
-    testMatch: ['<rootDir>/src/**/*.test.ts'],
+    testMatch: [
+        '<rootDir>/src/**/*.test.ts',
+        '<rootDir>/test/**/*.test.ts',
+    ],
     testEnvironment: 'node',
     clearMocks: true,
     preset: 'ts-jest',
---
+++ test/hello.test.ts
@@ -0,0 +1,3 @@
+test("Hello World! Jest ;)", function () {
+   console.log("Hello World! Jest");
+});
